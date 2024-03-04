b8952eb897a540db0fd62fb19aa422f19695330a
GregBiglione <gregory.biglione81@gmail.com>
2024-03-04 18:24:16 +0100

    Added SplashScreen, RegisterScreen & RegisterContent

---
+++ lib/presentation/screen/auth/register/register_screen.dart
@@ -0,0 +1,16 @@
+import 'package:email_auth_app/presentation/screen/auth/register/widget/register_content.dart';
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const RegisterContent();
+  }
+}
---
+++ lib/presentation/screen/auth/register/widget/register_content.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RegisterContent extends StatelessWidget {
+  const RegisterContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Register Screen",
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
