53ea637d5341d3c74995a9aa802aff00dab8c66a
GregBiglione <gregory.biglione81@gmail.com>
2024-03-10 20:19:24 +0100

    Added Injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:email_auth_app/app/di/injection.dart';
 
 import 'package:email_auth_app/presentation/resource/route_manager.dart';
 import 'package:email_auth_app/presentation/resource/theme_manager.dart';
@@ -7,9 +6,7 @@ import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await configureDependencies();
   runApp(const MyApp());
 }
 
