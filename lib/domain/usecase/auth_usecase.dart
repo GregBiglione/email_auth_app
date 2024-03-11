df6b9b55399bf3c7f5c518a01aece2533834530b
GregBiglione <gregory.biglione81@gmail.com>
2024-03-11 18:29:37 +0100

    Added RegisterUseCase & AuthUseCase

---
+++ lib/domain/usecase/auth_usecase.dart
@@ -0,0 +1,7 @@
+import 'package:email_auth_app/domain/usecase/register_usecase.dart';
+
+class AuthUseCase {
+  final RegisterUseCase registerUseCase;
+
+  AuthUseCase({required this.registerUseCase});
+}
