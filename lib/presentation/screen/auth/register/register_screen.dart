40a6bd53b8c3fcc70d039f8129934f9c9517c5f6
GregBiglione <gregory.biglione81@gmail.com>
2024-03-13 17:21:23 +0100

    Added StreamBuilder in RegisterScreen

--- lib/presentation/screen/auth/register/register_screen.dart
+++ lib/presentation/screen/auth/register/register_screen.dart
@@ -1,8 +1,14 @@
+import 'package:email_auth_app/app/constant.dart';
+import 'package:email_auth_app/app/function.dart';
 import 'package:email_auth_app/presentation/resource/color_manager.dart';
+import 'package:email_auth_app/presentation/resource/string_manager.dart';
 import 'package:email_auth_app/presentation/screen/auth/register/register_viewmodel.dart';
 import 'package:email_auth_app/presentation/screen/auth/register/widget/register_content.dart';
+import 'package:email_auth_app/presentation/screen/loading/loading_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../../../../domain/utils/state_render.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -16,7 +22,29 @@ class RegisterScreen extends StatelessWidget {
         backgroundColor: ColorManager.black,
         body: Center(
           child: SingleChildScrollView(
-            child: RegisterContent(viewModel: viewModel,),
+            child: StreamBuilder(
+              stream: viewModel.response,
+              builder: (context, snapshot) {
+                final response = snapshot.data;
+
+                if(response is Success) {
+                  logger.i("User data: $response");
+                  goToHomeScreen(context);
+                }
+                else if(response is Loading) {
+                  logger.w("Loading...");
+                  return const LoadingScreen();
+                }
+                else if(response is Error) {
+                  final data = response;
+                  String error = StringManager.error + data.error;
+
+                  errorToast(context, error);
+                }
+
+                return RegisterContent(viewModel: viewModel,);
+              },
+            ),
           ),
         ),
       ),
