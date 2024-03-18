09fd74282f66afc218335941636b4dc881bbde6a
GregBiglione <gregory.biglione81@gmail.com>
2024-03-18 18:30:24 +0100

    Added LoginState & LoginUseCase

--- lib/domain/usecase/auth_usecase.dart
+++ lib/domain/usecase/auth_usecase.dart
@@ -1,7 +1,10 @@
 import 'package:email_auth_app/domain/usecase/register_usecase.dart';
 
+import 'login_usecase.dart';
+
 class AuthUseCase {
   final RegisterUseCase registerUseCase;
+  final LoginUseCase loginUseCase;
 
-  AuthUseCase({required this.registerUseCase});
+  AuthUseCase({required this.registerUseCase, required this.loginUseCase});
 }
