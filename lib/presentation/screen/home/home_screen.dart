2b0a62b9b55f7d74265dfaa9ecfbce2bf5ff1ba8
GregBiglione <gregory.biglione81@gmail.com>
2024-03-21 19:33:09 +0100

    Added AppBar in HomeScreen

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,6 +1,11 @@
 import 'package:email_auth_app/presentation/resource/color_manager.dart';
+import 'package:email_auth_app/presentation/resource/font_manager.dart';
+import 'package:email_auth_app/presentation/resource/string_manager.dart';
+import 'package:email_auth_app/presentation/resource/style_manager.dart';
 import 'package:email_auth_app/presentation/screen/home/widget/home_content.dart';
 import 'package:flutter/material.dart';
+
+import '../../../app/constant.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,6 +13,27 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ColorManager.primary,
+        title: Text(
+          StringManager.homeTitle,
+          style: getBoldStyle(
+            fontSize: FontSize.s16,
+            color: ColorManager.white,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              logger.i("Logout clicked");
+            },
+            icon: Icon(
+              Icons.logout,
+              color: ColorManager.white,
+            ),
+          ),
+        ],
+      ),
       backgroundColor: ColorManager.black,
       body: const HomeContent(),
     );
