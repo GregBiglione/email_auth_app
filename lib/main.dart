5783404096469d606b84e1553afbdd2596064daa
GregBiglione <gregory.biglione81@gmail.com>
2024-03-22 14:16:11 +0100

    Added AuthUseCase in main

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,26 @@ import 'package:email_auth_app/presentation/screen/auth/register/register_viewmo
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'app/constant.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies();
-  runApp(const MyApp());
+  runApp(MyApp(getIt<AuthUseCase>()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final AuthUseCase _authUseCase;
+
+  const MyApp(this._authUseCase, {super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final String sessionId = _authUseCase.userSessionUseCase.userSession?.uid ?? "Not connected";
+
+    logger.i("User session id $sessionId");
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
