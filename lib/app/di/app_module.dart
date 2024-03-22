10a70547e1f3103bbb2a45d56c067cb43b7a70c5
GregBiglione <gregory.biglione81@gmail.com>
2024-03-22 17:41:44 +0100

    Added LogoutUseCase

--- lib/app/di/app_module.dart
+++ lib/app/di/app_module.dart
@@ -2,6 +2,7 @@ import 'package:email_auth_app/data/repository/auth_repository_implementer.dart'
 import 'package:email_auth_app/domain/repository/auth_repository.dart';
 import 'package:email_auth_app/domain/usecase/auth_usecase.dart';
 import 'package:email_auth_app/domain/usecase/login_usecase.dart';
+import 'package:email_auth_app/domain/usecase/logout_usecase.dart';
 import 'package:email_auth_app/domain/usecase/register_usecase.dart';
 import 'package:email_auth_app/domain/usecase/reset_password_usecase.dart';
 import 'package:email_auth_app/domain/usecase/user_session_usecase.dart';
@@ -31,5 +32,6 @@ abstract class AppModule {
     registerUseCase: RegisterUseCase(authRepository), 
     loginUseCase: LoginUseCase(authRepository), 
     resetPasswordUseCase: ResetPasswordUseCase(authRepository), 
+    logoutUseCase: LogoutUseCase(authRepository), 
   );
 }
